65050e057e616100ce465a1b716c7eaa4483bf10
mycompany <mycompany@DESKTOP-3803RVH>
2017-06-27 16:01:12 +0530

    welcome controller

---
+++ WebUser/src/main/java/com/aartekhotel/controller/WelcomeController.java
@@ -0,0 +1,21 @@
+package com.aartekhotel.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.aartek.models.Login;
+
+@Controller
+public class WelcomeController {
+	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
+	public String login(ModelMap map, HttpSession seHttpSession) {
+		// seHttpSession.invalidate();
+		return "welcome";
+	}
+	
+
+}
