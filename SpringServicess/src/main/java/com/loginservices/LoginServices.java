0fda93ed109e8e5411267055a8bc1c036c19fade
mycompany <mycompany@DESKTOP-3803RVH>
2017-06-27 17:49:26 +0530

    registrations

--- SpringServicess/src/main/java/com/loginservices/LoginServices.java
+++ SpringServicess/src/main/java/com/loginservices/LoginServices.java
@@ -1,5 +1,19 @@
 package com.loginservices;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aartek.models.Login;
+import com.aartek.repository.LoginRepository;
+@Service
 public class LoginServices {
+ 
+  @Autowired
+  LoginRepository loginRepository;
+	
+	public Login findByUserNameAndEmailId(Login login) {
+		// TODO Auto-generated method stub
+		return loginRepository.findByUserNameAndEmailIdR(login);
+	}
 
 }
---
+++ SpringServicess/src/main/java/com/loginservices/RegistrationServices.java
@@ -0,0 +1,22 @@
+package com.loginservices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aartek.models.Login;
+import com.aartek.repository.LoginRepository;
+import com.aartek.repository.RegistrationRepository;
+@Service
+public class RegistrationServices {
+
+	@Autowired
+	RegistrationRepository loginRepository;
+	
+	public boolean saveRegistrationform(Login login) {
+		   return loginRepository.saveRegistration(login);
+	}
+	
+	
+	
+
+}
